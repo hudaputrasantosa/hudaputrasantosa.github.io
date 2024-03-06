6ddc64832dfe6043ded0409f41a28681e290a4ec
hudaputrasantosa <hudaputrasantosa@gmail.com>
2024-03-06 16:37:37 +0700

    add: footer section

--- src/components/ContentLayout/index.jsx
+++ src/components/ContentLayout/index.jsx
@@ -1,12 +1,14 @@
+import Footer from "../Footer";
 import Navbar from "../Nav";
 
 const ContentLayout = ({ children }) => {
   return (
-    <div className="min-h-screen  bg-gray-50 dark:bg-slate-900 dark:text-white">
+    <div className="min-h-screen flex flex-col  bg-gray-50 dark:bg-slate-900 dark:text-white">
       <Navbar />
       <section className="flex flex-col w-full h-fit justify-center">
-        <div className="items-center lg:px-[25%] px-4 py-8">{children}</div>
+        <div className="items-center lg:px-[25%] px-6 py-8">{children}</div>
       </section>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,19 @@
+import { FaCode } from "react-icons/fa6";
+const year = new Date().getFullYear();
+const Footer = () => {
+  return (
+    <footer className="w-full mx-auto mt-auto py-4 border-t dark:border-slate-700">
+      <div className="flex items-center justify-center gap-2 font-semibold text-slate-700 dark:text-slate-300 lg:text-sm text-xs">
+        <p>Build</p>
+        <FaCode size="18" />
+        <p>
+          with 💖 by <span className="font-bold">Huda Putra Santosa</span>.
+          &copy;
+          {year}
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
