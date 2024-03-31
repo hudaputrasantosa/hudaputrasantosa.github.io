2bb813e373e81e69aa2c9712ef1badb63df78a6e
hudaputrasantosa <hudaputrasantosa@gmail.com>
2024-04-01 00:01:50 +0700

    updt: vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {},
-  },
   base: "/",
 });
