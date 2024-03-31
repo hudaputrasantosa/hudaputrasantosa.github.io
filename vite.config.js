111254dd7442b5c5aa6c3c6bb224bcfa4470bf98
hudaputrasantosa <hudaputrasantosa@gmail.com>
2024-04-01 00:20:09 +0700

    updt: fix chunk when build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,21 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/",
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
