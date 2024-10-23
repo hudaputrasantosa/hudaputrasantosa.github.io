47e8c07d506469aae848393aba928f521b1a6d42
hudaputrasantosa <hudaputrasantosa@gmail.com>
2024-10-23 23:48:11 +0700

    feat: add GA4

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Router from "./routes/index.jsx";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(import.meta.env.VITE_REACT_GA4_MEASUREMENT_ID);
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+  title: "Huda Pages",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
