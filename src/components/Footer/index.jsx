797fd218e7309ddfc3484c48f36bc46bab2b9921
hudaputrasantosa <hudaputrasantosa@gmail.com>
2025-02-11 21:23:09 +0700

    fix: update visitor badge and footer color

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import { FaCode } from "react-icons/fa6";
 const year = new Date().getFullYear();
 const Footer = () => {
   return (
-    <footer className="w-full mx-auto mt-auto py-4 border-t dark:border-slate-700">
+    <footer className="w-full mx-auto mt-auto py-4 border-t bg-gray-50 dark:border-slate-700">
       <div className="flex items-center justify-center gap-2 font-semibold text-slate-700 dark:text-slate-300 lg:text-sm text-xs">
         <p>Build</p>
         <FaCode size="18" />
